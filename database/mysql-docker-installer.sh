5a68b9b742580d2c642fa108aba0b5e735fd0cd7
Juan Arellano <juanarellano1996@gmail.com>
2020-11-06 19:22:47 +0100

    Mysql image starts automatically

--- database/mysql-docker-installer.sh
+++ database/mysql-docker-installer.sh
@@ -32,7 +32,7 @@ echo "Installing mysql/mysql-server:5.7 docker image"
 docker pull mysql/mysql-server:5.7
 
 echo "Creating instance"
-docker run -p 3306:3306 --name mysql1 -d mysql/mysql-server:5.7
+docker run -p 3306:3306 --name mysql1 -d --restart unless-stopped mysql/mysql-server:5.7
 
 
 docker logs mysql1 >> mysql-docker.log
